c1827b484ba9b31fd9d193a061b79beba28abcfc
ElHazard <pupline778@gmail.com>
2018-01-09 18:15:42 +0800

    add base file and base css framework(pure 1.0.0)

--- AFISS_CORE/src/main/java/org/oy/afiss/Main.java
+++ AFISS_CORE/src/main/java/org/oy/afiss/Main.java
@@ -1,5 +1,22 @@
 package org.oy.afiss;
 
-public class Main {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Main {
+	
+	@RequestMapping("hello")
+	public ModelAndView handleLogin(HttpServletRequest req,
+			HttpServletResponse resp) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("helloWorld");
+		return mav;
+	}
 }
